716a8f0c900352b8a83ca6f7823245afed3f686c
Stewart <stewart@toru.digital>
2021-05-17 22:25:20 +0100

    Save text value in state (Every keystroke))

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -1,11 +1,19 @@
-
+import React, {useState, useEffect} from 'react'
 import './App.scss'
 
 const App = () => {
+  const [text, setText] = useState('')
+
+  const handleChange = (event) => {
+    event.preventDefault()
+    let newText = event.target.value
+    setText(newText)
+  }
+
   return (
     <div className="container">
       <header>How Fast Can You Type!?</header>
-      <textarea name="" id=""/>
+      <textarea onChange={handleChange} value={text} name="" id=""/>
       <h2>Time Remaining: ???</h2>
       <button className="btn">Start</button>
     </div>
