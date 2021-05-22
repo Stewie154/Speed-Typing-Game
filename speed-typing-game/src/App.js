4ccdf615164b41cd8a90bc8ce7efee9a50a3ae94
Stewart <stewart@toru.digital>
2021-05-22 13:08:00 +0100

    useEffect
    clicking button makes timer count down to 0, then resets it to 5

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -5,6 +5,19 @@ const App = () => {
   const [text, setText] = useState('')
   const [wordCount, setWordCount] = useState(0)
   const [timeRemaining, setTimeRemaining] = useState(5)
+  const [gameOn, setGameOn] = useState(false)
+
+  useEffect( () => {
+    if (timeRemaining > 0 && gameOn) {
+      setTimeout( () => {
+        setTimeRemaining( prevTime => prevTime - 1 )
+      }, 1000 )
+    }
+    else if (timeRemaining === 0) {
+      setGameOn(false)
+      setTimeRemaining(5)
+    }
+  }, [timeRemaining, gameOn])
 
   const handleChange = (event) => {
     event.preventDefault()
@@ -18,12 +31,16 @@ const App = () => {
     setWordCount(total)
   }
 
+  const startGame = () => {
+    setGameOn(true)
+  }
+
   return (
     <div className="container">
       <header>How Fast Can You Type!?</header>
       <textarea onChange={handleChange} value={text} name="" id=""/>
       <h2>Seconds Remaining: {timeRemaining}</h2>
-      <button className="btn" onClick={() => countWords()}>Start</button>
+      <button className="btn" onClick={startGame}>Start</button>
       <h2>Your Word Count: {wordCount}</h2>
     </div>
   )
